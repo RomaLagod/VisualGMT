f43ce1ec3201571849e3cf6fb0eb9b5713fc1bbf
Roman Lahodniuk <RomaLagod@Gmail.com>
2019-04-07 23:43:49 +0300

    Added structure for interfaces

---
+++ GMT_GUI_component/ComponentInterface/IGMT_FATabStripCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMT_GUI_component.ComponentInterface
+{
+    public interface IGMT_FATabStripCollection
+    {
+    }
+}

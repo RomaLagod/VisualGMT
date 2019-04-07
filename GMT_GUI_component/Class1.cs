b4f8ad3dae71d9727bb0042cf5a176cdc0f647f9
Roman Lahodniuk <RomaLagod@Gmail.com>
2019-04-07 12:29:06 +0300

    Empty WindowsForm Application

---
+++ GMT_GUI_component/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMT_GUI_component
+{
+    public class Class1
+    {
+    }
+}
